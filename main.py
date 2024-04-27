48f639494244a3d7dbe2ec91f4914ac7be3728a4
Mikael-R <mikarg9@gmail.com>
2024-04-27 09:53:49 -0300

    feat: change active menu

--- main.py
+++ main.py
@@ -45,149 +45,161 @@ DB = {
     ],
 }
 
-MAIN_MENU = [
-    {
-        'title': 'Gerenciar usuários',
-        'code': 'users_menu',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Gerenciar filmes',
-        'code': 'films_menu',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Comprar ingresso',
-        'code': 'store_sale',
-        'roles': ['client'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Listar ingressos comprados',
-        'code': 'index_sales',
-        'roles': ['client'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Listar ingressos vendidos',
-        'code': 'index_sales',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Criar conta',
-        'code': 'register_user',
-        'roles': [],
-        'need_auth': False,
-        'need_no_auth': True
-    },
-    {
-        'title': 'Login',
-        'code': 'login',
-        'roles': [],
-        'need_auth': False,
-        'need_no_auth': True
-    },
-    {
-        'title': 'Deslogar',
-        'code': 'logout',
-        'roles': [],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Sair',
-        'code': 'exit',
-        'roles': [],
-        'need_auth': False,
-        'need_no_auth': False
-    }
-]
+MAIN_MENU = {
+    'title': 'Menu Principal',
+    'options': [
+        {
+            'title': 'Gerenciar usuários',
+            'code': 'users_menu',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Gerenciar filmes',
+            'code': 'films_menu',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Comprar ingresso',
+            'code': 'store_sale',
+            'roles': ['client'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Listar ingressos comprados',
+            'code': 'index_sales',
+            'roles': ['client'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Listar ingressos vendidos',
+            'code': 'index_sales',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Criar conta',
+            'code': 'register_user',
+            'roles': [],
+            'need_auth': False,
+            'need_no_auth': True
+        },
+        {
+            'title': 'Login',
+            'code': 'login',
+            'roles': [],
+            'need_auth': False,
+            'need_no_auth': True
+        },
+        {
+            'title': 'Deslogar',
+            'code': 'logout',
+            'roles': [],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Sair',
+            'code': 'exit',
+            'roles': [],
+            'need_auth': False,
+            'need_no_auth': False
+        }
+    ]
+}
 
-USER_MENU = [
-    {
-        'title': 'Listar usuários',
-        'code': 'index_user',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Cadastrar usuário',
-        'code': 'store_user',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Editar usuário',
-        'code': 'update_user',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Apagar usuário',
-        'code': 'destroy_user',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Voltar',
-        'code': 'back',
-        'roles': [],
-        'need_auth': False,
-        'need_no_auth': False
-    }
-]
+USER_MENU = {
+    'title': 'Menu de Gerenciamento de Usuários',
+    'options': [
+        {
+            'title': 'Listar usuários',
+            'code': 'index_user',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Cadastrar usuário',
+            'code': 'store_user',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Editar usuário',
+            'code': 'update_user',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Apagar usuário',
+            'code': 'destroy_user',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Voltar',
+            'code': 'back',
+            'roles': [],
+            'need_auth': False,
+            'need_no_auth': False
+        }
+    ]
+}
 
-FILM_MENU = [
-    {
-        'title': 'Listar filmes',
-        'code': 'index_film',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Cadastrar filme',
-        'code': 'store_film',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Editar filme',
-        'code': 'update_filme',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Apagar filme',
-        'code': 'destroy_film',
-        'roles': ['admin'],
-        'need_auth': True,
-        'need_no_auth': False
-    },
-    {
-        'title': 'Voltar',
-        'code': 'back',
-        'roles': [],
-        'need_auth': False,
-        'need_no_auth': False
-    }
-]
+FILM_MENU = {
+    'title': 'Menu de Gerenciamento de Filmes',
+    'options': [
+        {
+            'title': 'Listar filmes',
+            'code': 'index_film',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Cadastrar filme',
+            'code': 'store_film',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Editar filme',
+            'code': 'update_filme',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Apagar filme',
+            'code': 'destroy_film',
+            'roles': ['admin'],
+            'need_auth': True,
+            'need_no_auth': False
+        },
+        {
+            'title': 'Voltar',
+            'code': 'back',
+            'roles': [],
+            'need_auth': False,
+            'need_no_auth': False
+        }
+    ]
+}
 
-user_logged = None
+previous_menu = MAIN_MENU
+active_menu = MAIN_MENU
+
+user_logged = DB['users'][0]
 
 while True:
     print('\033c========== BEM VINDO AO CINE SERTÃO ==========')
@@ -195,45 +207,45 @@ while True:
     if (user_logged):
         print(f"\nOlá, {user_logged['name']}!")
 
-    print("\n* Menu Principal")
+    print(f"\n* {active_menu['title']}")
 
-    main_menu_valid_options = []
+    valid_menu_options = []
 
-    for option in MAIN_MENU:
+    for option in active_menu['options']:
         if user_logged == None:
             if (option['need_auth'] == True or len(option['roles'])):
                 continue
         else:
             if (option['need_no_auth'] == True or (len(option['roles']) and user_logged['type'] not in option['roles'])):
                 continue
-        main_menu_valid_options.append(option)
-        print(f"{len(main_menu_valid_options)}. {option['title']}")
+        valid_menu_options.append(option)
+        print(f"{len(valid_menu_options)}. {option['title']}")
 
-    main_menu_index = input("\nEscolha uma opção: ")
+    menu_index = input("\nEscolha uma opção: ")
 
-    if (not main_menu_index.isdigit() or
-        int(main_menu_index) < 1 or
-        int(main_menu_index) > len(main_menu_valid_options)
+    if (not menu_index.isdigit() or
+        int(menu_index) < 1 or
+        int(menu_index) > len(valid_menu_options)
     ):
         input("Opção inválida.\n\nPressione Enter para continuar...")
         continue
 
-    main_menu_option = main_menu_valid_options[int(main_menu_index) - 1]
+    menu_option = valid_menu_options[int(menu_index) - 1]
 
     print('')
 
-    if main_menu_option['code'] == 'films_menu':
-        print("* Menu de Gerenciamento de Filmes")
-        print(FILM_MENU)
+    if menu_option['code'] == 'films_menu':
+        active_menu = FILM_MENU
+        continue
 
-    elif main_menu_option['code'] == 'users_menu':
-        print("* Menu de Gerenciamento de Usuários")
-        print(USER_MENU)
+    elif menu_option['code'] == 'users_menu':
+        active_menu = USER_MENU
+        continue
 
-    elif main_menu_option['code'] == 'store_sale':
+    elif menu_option['code'] == 'store_sale':
         print("TODO: Listar filmes e permitir a compra de ingressos")
 
-    elif main_menu_option['code'] == 'index_sales':
+    elif menu_option['code'] == 'index_sales':
         print('-' * 20)
         for sale in DB['sales']:
             film = next((film for film in DB['films'] if film['id'] == sale['film_id']), None)
@@ -255,7 +267,7 @@ while True:
             print(f"Preço: R$ {film['price']:.2f}")
             print('-' * 20)
 
-    elif main_menu_option['code'] == 'register_user':
+    elif menu_option['code'] == 'register_user':
         new_user = {
             'id': str(random.random()),
             'type': 'client'
@@ -285,7 +297,7 @@ while True:
 
         print('Usuário cadastrado com sucesso!')
 
-    elif main_menu_option['code'] == 'login':
+    elif menu_option['code'] == 'login':
         while True:
             while True:
                 email = input("Digite o email: ")
@@ -307,11 +319,15 @@ while True:
             else:
                 print('Email ou senha inválidos!')
 
-    elif main_menu_option['code'] == 'logout':
+    elif menu_option['code'] == 'logout':
         print(f"Até mais, {user_logged['name']}!")
         user_logged = None
 
-    elif main_menu_option['code'] == 'exit':
+    elif menu_option['code'] == 'back':
+        active_menu = previous_menu
+        continue
+
+    elif menu_option['code'] == 'exit':
         name = 'usuário anônimo' if user_logged == None else user_logged['name']
         print(f'Até mais, {name}!')
         exit()
